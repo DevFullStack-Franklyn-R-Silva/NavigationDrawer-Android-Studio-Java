ceffbe2618dc676b778c6449c8440fe2f5536968
Franklyn R <franklyn1998@live.com>
2022-08-24 19:13:28 -0300

    Estrutura do NavigationDrawer

--- app/src/main/java/com/hadesfranklyn/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/navigationdrawer/MainActivity.java
@@ -1,19 +1,15 @@
 package com.hadesfranklyn.navigationdrawer;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.navigation.NavigationView;
 import com.hadesfranklyn.navigationdrawer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,27 +24,29 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Configura barra de navegacao
         setSupportActionBar(binding.appBarMain.toolbar);
 
-
+        // Cria referencia para toda a área do NavigationDrawer
         DrawerLayout drawer = binding.drawerLayout;
+
+        // Cria referencia para a área do NavigationDrawer
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Define configuracoes do NavigationDrawer
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Configura area que irá carregar os fragments
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        // Configura menu superior de navegação
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+        // Configura navegação para NavigationView
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
